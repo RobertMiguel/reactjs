5e1f9b10b37793233efa4f027d7b841804025878
RobertMiguel <robertresende9@gmail.com>
2022-11-18 23:16:19 -0300

    UseEffect02

--- skullGame/src/main.tsx
+++ skullGame/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import {Car} from './useState'
 import { App } from './useState'
 import { Time } from './useEffect';
+import { Counter } from './useEffect';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
     <Car />
     <Time />
+    <Counter />
   </React.StrictMode>
 )
