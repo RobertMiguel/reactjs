95d1c74858440aefa9f8aa6c7bc34c56c0b4ab35
RobertMiguel <90520597+RobertMiguel@users.noreply.github.com>
2023-03-23 23:09:20 -0300

    Update App.js

--- navbarfooter/src/App.js
+++ navbarfooter/src/App.js
@@ -9,6 +9,7 @@ import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Projects from './components/pages/Projects';
 
+
 function App() {
   return (
     <Router>
